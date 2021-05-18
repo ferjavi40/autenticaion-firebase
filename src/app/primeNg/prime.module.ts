e769c8c9100e753f7b4b97af2b0aafe71d26c2de
ferjavi40 <ferjaviramirez@gmail.com>
2021-05-18 14:03:25 +0200

    fix: header

--- src/app/primeNg/prime.module.ts
+++ src/app/primeNg/prime.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MenubarModule } from 'primeng/menubar';
+import {ButtonModule} from 'primeng/button';
+
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MenubarModule
+  ],
+  exports: [
+    MenubarModule,
+    ButtonModule
   ]
+
 })
 export class PrimeModule { }
