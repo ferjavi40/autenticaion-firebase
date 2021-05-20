fc4dd66aaadf2bfbaf8c6b219c65ad0b5f25f78a
ferjavi40 <ferjaviramirez@gmail.com>
2021-05-20 14:50:14 +0200

    feature: add register

--- src/app/auth/login/login.module.ts
+++ src/app/auth/login/login.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { PrimeModule } from 'src/app/primeNg/prime.module';
 
 
 @NgModule({
@@ -11,7 +13,8 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    PrimeModule
   ]
 })
 export class LoginModule { }
